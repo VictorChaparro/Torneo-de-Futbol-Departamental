6c873e735871f9f18d55c71bf02e0c597458ffce
unknown <victorchaparro615@gmail.com>
2021-10-07 14:16:38 -0500

    commit 2

--- TorFutDep.App.Dominio/Entidades/Novedad.cs
+++ TorFutDep.App.Dominio/Entidades/Novedad.cs
@@ -6,8 +6,8 @@ namespace TorFutDep.App.Dominio
         public int Id { get; set; }
         public Equipo N_Equipo { get; set; }
         public Jugador N_Jugador { get; set; }
+        public Partido N_Partido { get; set; }
         public DateTime FechaHora { get; set; }
         public Tipo_Novedad N_Tipo_Novedad { get; set; }
-        public Partido N_Partido { get; set; }
     }
 }
