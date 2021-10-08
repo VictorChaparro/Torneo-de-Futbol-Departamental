1e2ef0b70f4d488282edf32ee167ab61a569658d
unknown <victorchaparro615@gmail.com>
2021-10-08 16:07:50 -0500

    creacion de pages

--- TorFutDep.App.Frontend/Startup.cs
+++ TorFutDep.App.Frontend/Startup.cs
@@ -8,7 +8,6 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using TorFutDep.App.Persistencia;
 
 namespace TorFutDep.App.Frontend
 {
@@ -25,7 +24,6 @@ namespace TorFutDep.App.Frontend
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRazorPages();
-            services.AddSingleton<IRepositorioDirector_Tecnico, RepositorioDirector_Tecnico>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
