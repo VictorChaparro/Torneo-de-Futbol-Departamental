3878c8b5821b19195d368e457e2b22fb5958915c
unknown <victorchaparro615@gmail.com>
2021-10-08 16:57:55 -0500

    Details de Directores Técnicos

--- TorFutDep.App.Frontend/Pages/Directores_Tecnicos/Details.cshtml.cs
+++ TorFutDep.App.Frontend/Pages/Directores_Tecnicos/Details.cshtml.cs
@@ -5,12 +5,30 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
+using TorFutDep.App.Dominio;
+using TorFutDep.App.Persistencia;
+
 namespace TorFutDep.App.Frontend.Pages.Directores_Tecnicos
 {
     public class DetailsModel : PageModel
     {
-        public void OnGet()
+        private readonly IRepositorioDirector_Tecnico _repoDirector_tecnico;
+        public Director_Tecnico director_tecnico { get; set; }
+        public DetailsModel(IRepositorioDirector_Tecnico repoDirector_tecnico)
         {
+            _repoDirector_tecnico = repoDirector_tecnico;
+        }
+        public IActionResult OnGet(int id)
+        {
+            director_tecnico = _repoDirector_tecnico.GetDirector_Tecnico(id);
+            if(director_tecnico == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                return Page();
+            }
         }
     }
 }
