8c2159c82c34ab3067e6628e07040a9e38bf1b06
unknown <victorchaparro615@gmail.com>
2021-10-08 16:27:05 -0500

    Index de Directores Técnicos

--- TorFutDep.App.Frontend/Pages/Directores_Tecnicos/Index.cshtml.cs
+++ TorFutDep.App.Frontend/Pages/Directores_Tecnicos/Index.cshtml.cs
@@ -4,13 +4,22 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using TorFutDep.App.Dominio;
+using TorFutDep.App.Persistencia;
 
 namespace TorFutDep.App.Frontend.Pages.Directores_Tecnicos
 {
     public class IndexModel : PageModel
     {
+        private readonly IRepositorioDirector_Tecnico _repoDirector_tecnico;
+        public IEnumerable<Director_Tecnico> directores_tecnicos { get; set; }
+        public IndexModel(IRepositorioDirector_Tecnico repoDirector_tecnico)
+        {
+            _repoDirector_tecnico = repoDirector_tecnico;
+        }
         public void OnGet()
         {
+            directores_tecnicos = _repoDirector_tecnico.GetAllDirectores_Tecnicos();
         }
     }
 }
