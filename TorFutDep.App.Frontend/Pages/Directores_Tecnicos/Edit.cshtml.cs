9166aa64c3e84645ca3236f89668dddf775ccd96
unknown <victorchaparro615@gmail.com>
2021-10-09 08:06:45 -0500

    Editar Director Técnico

--- TorFutDep.App.Frontend/Pages/Directores_Tecnicos/Edit.cshtml.cs
+++ TorFutDep.App.Frontend/Pages/Directores_Tecnicos/Edit.cshtml.cs
@@ -4,13 +4,35 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using TorFutDep.App.Dominio;
+using TorFutDep.App.Persistencia;
 
 namespace TorFutDep.App.Frontend.Pages.Directores_Tecnicos
 {
     public class EditModel : PageModel
     {
-        public void OnGet()
+        private readonly IRepositorioDirector_Tecnico _repoDirector_tecnico;
+        public Director_Tecnico director_tecnico { get; set; }
+        public EditModel(IRepositorioDirector_Tecnico repoDirector_tecnico)
         {
+            _repoDirector_tecnico = repoDirector_tecnico;
+        }
+        public IActionResult OnGet(int id)
+        {
+            director_tecnico = _repoDirector_tecnico.GetDirector_Tecnico(id);
+            if(director_tecnico == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                return Page();
+            }
+        }
+        public IActionResult OnPost(Director_Tecnico director_Tecnico)
+        {
+            _repoDirector_tecnico.UpdateDirector_Tecnico(director_Tecnico);
+            return RedirectToPage("Index");
         }
     }
 }
